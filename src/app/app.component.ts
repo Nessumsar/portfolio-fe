93aae7dc485cc6b91d49d5b9903b89d0db806a56
Nessumsar <lukasrasmusseen@gmail.com>
2025-04-11 12:17:34 +0200

    About component removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ResumeComponent } from './components/resume/resume.component';
 import { ContactComponent } from './components/contact/contact.component';
@@ -14,7 +13,6 @@ import { FooterComponent } from './components/footer/footer.component';
     RouterOutlet,
     HeaderComponent,
     HomeComponent,
-    AboutComponent,
     ProjectsComponent,
     ResumeComponent,
     ContactComponent,
