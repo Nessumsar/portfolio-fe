59c5ee660f06e6104dd293574292e38941bfa564
Nessumsar <lukasrasmusseen@gmail.com>
2025-04-22 13:23:25 +0200

    Getting auth from environment

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,24 +1,25 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
 
 @Injectable({
     providedIn: 'root'
   })
 export class ApiService {
   private http = inject(HttpClient);
-  private apiUrl = 'http://localhost:8080/api';
-  private token = 'todo';
+  private apiUrl = '/api';
+  private token = environment.apiKey;
 
   constructor() {}
 
   get(endpoint: string): Observable<any> {
-    /*
-    headers: {
-        'Authorization': 'Bearer '+this.token
+    return this.http.get<any[]>(`${this.apiUrl}/${endpoint}`, {
+      headers: {
+        'X-API-KEY': this.token
       }
-        */
-    return this.http.get<any[]>(`${this.apiUrl}/${endpoint}`);
+    });
   }
 
 }
